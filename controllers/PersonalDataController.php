b9870d2b298196aaba070432e7aa24731a8abe22
ustupan <andrzej@MacBook-Pro-Andrzej.local>
2019-01-19 14:43:10 +0100

    Users details controller

---
+++ controllers/PersonalDataController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrzej
+ * Date: 2019-01-18
+ * Time: 21:26
+ */
+
+class PersonalDataContoller
+{
+
+}
--- views/PavingController/generator.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: andrzej
- * Date: 2019-01-13
- * Time: 16:38
- */
